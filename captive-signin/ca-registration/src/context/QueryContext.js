664d9fac752a0e56fcb6a735d8f3e911ffa1ddef
Toroyteach <tonny35toro@gmail.com>
2025-04-04 13:44:34 +0300

    fix ui registration

--- captive-signin/ca-registration/src/context/QueryContext.js
+++ captive-signin/ca-registration/src/context/QueryContext.js
@@ -1,25 +1,23 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import Cookies from "js-cookie";
 
 const QueryContext = createContext();
 
 export const QueryProvider = ({ children }) => {
     const location = useLocation();
     const [queryParams, setQueryParams] = useState(() => {
-        const storedParams = Cookies.get("queryParams");
-        return storedParams ? JSON.parse(storedParams) : {};
+        return {};
     });
 
     useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const params = Object.fromEntries(searchParams.entries());
-
-        if (Object.keys(params).length > 0) {
-            setQueryParams(params);
-            Cookies.set("queryParams", JSON.stringify(params), { expires: 1 / 288 }); // 5 min expiry
+        if (Object.keys(queryParams).length === 0) { 
+            const searchParams = new URLSearchParams(location.search);
+            const params = Object.fromEntries(searchParams.entries());
+            if (Object.keys(params).length > 0) {
+                setQueryParams(params);
+            }
         }
-    }, [location.search]);
+    }, [location.search, queryParams]);
 
     return (
         <QueryContext.Provider value={queryParams}>
